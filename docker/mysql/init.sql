f33f8296e475304535c0bac6507386c1a691b7d6
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-10-25 19:05:15 -0300

    feat: configurando tables no docker-compose

---
+++ docker/mysql/init.sql
@@ -0,0 +1,21 @@
+create database revvo_desafio;
+
+use revvo_desafio;
+
+create table users
+(
+    id           int auto_increment primary key,
+    name         varchar(255) not null,
+    email        varchar(255) not null,
+    password     varchar(255) not null,
+    avatar       varchar(255) not null,
+    first_access boolean default 0 null
+);
+
+create table courses
+(
+    id          int auto_increment primary key,
+    title       varchar(255) not null,
+    description text         not null,
+    banner      varchar(255) not null
+);
